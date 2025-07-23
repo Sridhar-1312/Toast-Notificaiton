eaed64a0e7568d8e1818598d3311c2e9b48652d0
Sridhar-1312 <ronaldosriiicr7@gmail.com>
2025-07-23 23:51:23 +0530

    completed

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-let container = document.querySelector(".container");
-let toastBox = document.getElementById("toastBox")
+let toastBox = document.getElementById('toastBox');
 
+let successMsg = '<i class="fa-solid fa-circle-check"></i>  Successfully submitted.'
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i>  Please fix the error!.'
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i>  invalid input, Check again. '
 
-showToast =  (()=>{
-    let toast  =  document.createElement("div");
+function showToast(msg) {
+    const toast = document.createElement('div');
     toast.classList.add('toast');
-    toast.innerHTML= "success";
+    toast.innerHTML = msg;
+    toastBox.appendChild(toast);
 
-})
+    if(msg.includes('error')){
+            toast.classList.add('error');
+
+    }
+    if(msg.includes('invalid')){
+            toast.classList.add('invalid');
+
+    }
+    setTimeout(()=>{
+        toast.remove();
+    },6000)
+}
+
